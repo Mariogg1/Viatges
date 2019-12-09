cb4e857b75ec75b8fdcb2baea9d9fb1a8c1ff8fb
Aleix <a14alerevagu@inspedralbes.cat>
2019-12-09 12:20:37 +0100

    scripts sql

---
+++ README.md.txt
@@ -0,0 +1,9 @@
+Numero de GRUP: GRUP 4
+Integrants del projecte: Mario Gonzalez, Adam Jalich i Aleix Revesado
+Objectiu “breu” del projecte
+Estat “breu” del projecte
+Adreça del repositori (loopback :-D): https://github.com/Mariogg1/Viatges
+Adreça del Jira: http://labs.iam.cat:8080/secure/RapidBoard.jspa?projectKey=VIAT3&rapidView=3
+Adreça web de la documentació phpdoc (labs.iam.cat)
+Adreça web del projecte desplegat (labs.iam.cat)
+Observacions respecte a la seguretat
---
+++ creacioDB.sql
@@ -0,0 +1,46 @@
+DROP DATABASE IF EXISTS viatges;
+
+CREATE DATABASE viatges;
+
+USE viatges;
+
+CREATE TABLE experiencies(
+	id INT(5) AUTO_INCREMENT PRIMARY KEY,
+    titol VARCHAR (50) NOT NULL,
+    dataPublicacio DATETIME DEFAULT CURRENT_TIMESTAMP,
+    text VARCHAR(250) NOT NULL,
+    imatge LONGBLOB NOT NULL,
+    lat FLOAT( 10, 6 ) NOT NULL ,
+	lng FLOAT( 10, 6 ) NOT NULL ,
+    valoracionsPositives INT(4) DEFAULT 0,
+    valoracionsNegatives INT(4) DEFAULT 0,
+    estat INT CHECK(estat=0 || estat=1),
+    idCategoria INT(2),
+    idUsuari INT(5),
+    FOREIGN KEY (idCategoria) REFERENCES categories(id)
+    ON UPDATE CASCADE ON DELETE SET NULL,
+    FOREIGN KEY (idUsuari) REFERENCES usuaris(id)
+    ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+CREATE TABLE usuaris(
+	id INT(5) AUTO_INCREMENT PRIMARY KEY,
+    nomUsuari VARCHAR (10) NOT NULL,
+    contrasenya VARCHAR (15) NOT NULL,
+	nom VARCHAR (20) NOT NULL,
+	primerCognom VARCHAR (20) NOT NULL,
+    segonCognom VARCHAR (20) NOT NULL,
+    correuElectronic VARCHAR(50) NOT NULL,
+    estat INT CHECK(estat=0 || estat=1)
+);
+
+CREATE TABLE categories(
+	id INT(2) AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR (20) NOT NULL
+);
+
+CREATE TABLE administradors(
+	idUsuari INT(5),
+    FOREIGN KEY (idUsuari) REFERENCES usuaris (id)
+    ON UPDATE CASCADE ON DELETE SET NULL
+);
