aa89ec1f67928d59bcd7283242f64819bc250005
a18adajaljal <a18adajaljal@inspedralbes.cat>
2019-12-10 08:49:50 +0100

    creacio footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="copyright">
+        <hr width="80%"/>
+        <h3>Pedralbes experiencies</h3>
+    </div>
+    <div>
+        <p>Telèfon: 932 033 332 | Horari: 9:00 a 13:00 hores | E-mail: ************ | Copyright © 2019</p>
+    </div>
+</footer>
