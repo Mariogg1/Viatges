e2681337f2ebf16c105d7972aeecfec79eba2e2b
a18adajaljal <a18adajaljal@inspedralbes.cat>
2019-12-10 08:35:42 +0100

    carpeta js

---
+++ js/documents.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    document.getElementById("login-select").addEventListener("click", function(){
+        document.getElementById("login-register").style.display="none";
+        document.getElementById("login-form").style.display="block";
+     })
+}
