3951647b7d3f3eaca0a63eaeed320f47da67107c
a18adajaljal <a18adajaljal@inspedralbes.cat>
2019-12-10 12:55:28 +0100

    style i buscador

---
+++ buscador.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    </head>
+    <body>
+    <form>
+        <div class="form-group">
+        Titol: <br><input type="text" name="titol"></input><br>
+        <label for="comment">Text:</label>
+        <textarea class="form-control" rows="5" id="comment"></textarea>
+        Imatge:
+        <input type="file" class="form-control-file" id="exampleFormControlFile1"><br>
+        <input type="submit" ></input>
+        </div>
+    </form>
+    </body>
+</html>
--- recarga.php
+++ recarga.php
@@ -101,6 +101,7 @@
                 <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
               </div>
             </div>
+            <button type="button" class="btn btn-primary" onclick="location.href='index.php'">Torna a la pagina d'inici</button>
             <hr class="featurette-divider">
             <!-- /END THE FEATURETTES -->
             <!-- FOOTER -->
