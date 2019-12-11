3e636553e7b413ea7a221210c3d4cc616b2e89fb
Aleix <a14alerevagu@inspedralbes.cat>
2019-12-11 10:33:49 +0100

    edicio scripts sql

--- creacioDB.sql
+++ creacioDB.sql
@@ -8,13 +8,11 @@ CREATE TABLE experiencies(
 	id INT(5) AUTO_INCREMENT PRIMARY KEY,
     titol VARCHAR (50) NOT NULL,
     dataPublicacio DATETIME DEFAULT CURRENT_TIMESTAMP,
-    text VARCHAR(250) NOT NULL,
+    text VARCHAR(2000) NOT NULL,
     imatge VARCHAR(50) NOT NULL,
-    lat FLOAT( 10, 6 ) NOT NULL ,
-	lng FLOAT( 10, 6 ) NOT NULL ,
     valoracionsPositives INT(4) DEFAULT 0,
     valoracionsNegatives INT(4) DEFAULT 0,
-    estat INT CHECK(estat=0 || estat=1),
+    estat INT DEFAULT 1 CHECK(estat=0 || estat=1),
     idCategoria INT(2),
     idUsuari INT(5),
     FOREIGN KEY (idCategoria) REFERENCES categories(id)
@@ -31,7 +29,7 @@ CREATE TABLE usuaris(
 	primerCognom VARCHAR (20) NOT NULL,
     segonCognom VARCHAR (20) NOT NULL,
     correuElectronic VARCHAR(50) NOT NULL,
-    estat INT CHECK(estat=0 || estat=1) DEFAULT 1
+    estat INT DEFAULT 1 CHECK(estat=0 || estat=1)
 );
 
 CREATE TABLE categories(
--- emplena.sql
+++ emplena.sql
@@ -9,16 +9,26 @@ VALUES("pep32", "pep1234", "Pep", "Garcia", "Ferrer", "pep32@hotmail.com", 1);
 
 INSERT INTO ADMINISTRADORS VALUES(1);
 
-INSERT INTO EXPERIENCIES (titol, text, imatge, lat, lng, estat, idCategoria, idUsuari)
-VALUES("Viatge a Roma", "Lorem Ipsum is simply dummy text of the printing.", 
-"roma.jpg", "41.895423", "12.500265", 1, 1, 1);
+INSERT INTO EXPERIENCIES (titol, text, imatge, estat, idCategoria, idUsuari)
+VALUES("Viatge a Roma", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sodales nisl sem, ac commodo turpis aliquet at. In porta sapien posuere, porta purus ac, sagittis lacus. Mauris feugiat arcu quis libero ultricies porta. Nulla fermentum mauris sed turpis ornare, et ultrices urna hendrerit. Aenean feugiat dapibus tellus, quis pellentesque quam. Duis lobortis velit quis magna semper ultricies. Sed ullamcorper pellentesque lectus, et placerat libero malesuada ac. Nulla mollis est et risus pulvinar facilisis. Phasellus euismod risus vitae augue scelerisque blandit. In tellus nisl, faucibus ut nibh vel, euismod tempor lorem. Nullam iaculis, nisi sit amet vehicula iaculis, sapien turpis sagittis massa, eget tincidunt elit sapien vitae turpis. Duis ante sem, pharetra at efficitur sit amet, pretium ut sapien.
 
-INSERT INTO EXPERIENCIES (titol, text, imatge, lat, lng, estat, idCategoria, idUsuari)
-VALUES("Viatge a París", "Lorem Ipsum is simply dummy text of the printing.", 
-"paris.jpg", "48.858393", "2.351511", 1, 2, 2);
+Mauris sit amet massa non mi imperdiet imperdiet vel in leo. Proin viverra viverra volutpat. Aenean vitae magna eu libero mattis tristique. Maecenas ac magna eget ex sagittis pretium et a leo. Sed ultricies leo eu tincidunt mollis. Vestibulum volutpat elit nec commodo sagittis. Sed id blandit nulla. Praesent congue consectetur faucibus. Nulla eget arcu ante. Maecenas fermentum tincidunt sem, non facilisis magna congue vitae. In suscipit non odio non vestibulum. Nam sodales nibh sed nunc faucibus elementum ac at dui. Nulla elementum molestie ante, quis posuere sem maximus nec. Mauris eu blandit erat, ac gravida libero. Praesent lacus mauris, commodo sit amet rhoncus sit amet, cursus sit amet nisi.
 
-INSERT INTO EXPERIENCIES (titol, text, imatge, lat, lng, estat, idCategoria, idUsuari)
-VALUES("Viatge a Londres", "Lorem Ipsum is simply dummy text of the printing.", 
-"londres.jpg", "51.485820", " -0.109844", 1, 1, 1);
+Aliquam ultricies urna eu purus egestas, et pellentesque quam dapibus. Fusce sodales nisi vitae vehicula suscipit. Aenean urna nulla, fermentum ut purus quis, lacinia pharetra nisl. Morbi at volutpat neque, ut commodo tellus. Fusce at tristique ipsum. Curabitur justo lacus, rhoncus et metus et, tempor tristique eros. Fusce vulputate ex vitae suscipit vehicula. Proin vitae purus quis eros sagittis pellentesque. Phasellus vel justo urna.", 
+"roma.jpg", 1, 1, 1);
 
-SELECT * FROM experiencies;
+INSERT INTO EXPERIENCIES (titol, text, imatge, estat, idCategoria, idUsuari)
+VALUES("Viatge a París", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sodales nisl sem, ac commodo turpis aliquet at. In porta sapien posuere, porta purus ac, sagittis lacus. Mauris feugiat arcu quis libero ultricies porta. Nulla fermentum mauris sed turpis ornare, et ultrices urna hendrerit. Aenean feugiat dapibus tellus, quis pellentesque quam. Duis lobortis velit quis magna semper ultricies. Sed ullamcorper pellentesque lectus, et placerat libero malesuada ac. Nulla mollis est et risus pulvinar facilisis. Phasellus euismod risus vitae augue scelerisque blandit. In tellus nisl, faucibus ut nibh vel, euismod tempor lorem. Nullam iaculis, nisi sit amet vehicula iaculis, sapien turpis sagittis massa, eget tincidunt elit sapien vitae turpis. Duis ante sem, pharetra at efficitur sit amet, pretium ut sapien.
+
+Mauris sit amet massa non mi imperdiet imperdiet vel in leo. Proin viverra viverra volutpat. Aenean vitae magna eu libero mattis tristique. Maecenas ac magna eget ex sagittis pretium et a leo. Sed ultricies leo eu tincidunt mollis. Vestibulum volutpat elit nec commodo sagittis. Sed id blandit nulla. Praesent congue consectetur faucibus. Nulla eget arcu ante. Maecenas fermentum tincidunt sem, non facilisis magna congue vitae. In suscipit non odio non vestibulum. Nam sodales nibh sed nunc faucibus elementum ac at dui. Nulla elementum molestie ante, quis posuere sem maximus nec. Mauris eu blandit erat, ac gravida libero. Praesent lacus mauris, commodo sit amet rhoncus sit amet, cursus sit amet nisi.
+
+Aliquam ultricies urna eu purus egestas, et pellentesque quam dapibus. Fusce sodales nisi vitae vehicula suscipit. Aenean urna nulla, fermentum ut purus quis, lacinia pharetra nisl. Morbi at volutpat neque, ut commodo tellus. Fusce at tristique ipsum. Curabitur justo lacus, rhoncus et metus et, tempor tristique eros. Fusce vulputate ex vitae suscipit vehicula. Proin vitae purus quis eros sagittis pellentesque. Phasellus vel justo urna.", 
+"paris.jpg", 1, 2, 2);
+
+INSERT INTO EXPERIENCIES (titol, text, imatge, estat, idCategoria, idUsuari)
+VALUES("Viatge a Londres", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sodales nisl sem, ac commodo turpis aliquet at. In porta sapien posuere, porta purus ac, sagittis lacus. Mauris feugiat arcu quis libero ultricies porta. Nulla fermentum mauris sed turpis ornare, et ultrices urna hendrerit. Aenean feugiat dapibus tellus, quis pellentesque quam. Duis lobortis velit quis magna semper ultricies. Sed ullamcorper pellentesque lectus, et placerat libero malesuada ac. Nulla mollis est et risus pulvinar facilisis. Phasellus euismod risus vitae augue scelerisque blandit. In tellus nisl, faucibus ut nibh vel, euismod tempor lorem. Nullam iaculis, nisi sit amet vehicula iaculis, sapien turpis sagittis massa, eget tincidunt elit sapien vitae turpis. Duis ante sem, pharetra at efficitur sit amet, pretium ut sapien.
+
+Mauris sit amet massa non mi imperdiet imperdiet vel in leo. Proin viverra viverra volutpat. Aenean vitae magna eu libero mattis tristique. Maecenas ac magna eget ex sagittis pretium et a leo. Sed ultricies leo eu tincidunt mollis. Vestibulum volutpat elit nec commodo sagittis. Sed id blandit nulla. Praesent congue consectetur faucibus. Nulla eget arcu ante. Maecenas fermentum tincidunt sem, non facilisis magna congue vitae. In suscipit non odio non vestibulum. Nam sodales nibh sed nunc faucibus elementum ac at dui. Nulla elementum molestie ante, quis posuere sem maximus nec. Mauris eu blandit erat, ac gravida libero. Praesent lacus mauris, commodo sit amet rhoncus sit amet, cursus sit amet nisi.
+
+Aliquam ultricies urna eu purus egestas, et pellentesque quam dapibus. Fusce sodales nisi vitae vehicula suscipit. Aenean urna nulla, fermentum ut purus quis, lacinia pharetra nisl. Morbi at volutpat neque, ut commodo tellus. Fusce at tristique ipsum. Curabitur justo lacus, rhoncus et metus et, tempor tristique eros. Fusce vulputate ex vitae suscipit vehicula. Proin vitae purus quis eros sagittis pellentesque. Phasellus vel justo urna.", 
+"londres.jpg", 1, 1, 1);
