437997f2e4c6abad7dd288e1bbbcedc3b6929d05
Aleix <a14alerevagu@inspedralbes.cat>
2019-12-17 13:15:03 +0100

    admin login

--- header-registrat.php
+++ header-registrat.php
@@ -1,11 +1,34 @@
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        <div class="col-md-7"></div>
-        <div class="col-md-1">
+<?php
+
+    $nomUsuari = $_SESSION['login_user'];
+
+    include_once 'conexioBD.php';
+
+    $sql = "SELECT id, nomUsuari FROM usuaris WHERE nomUsuari='$nomUsuari'";
+
+    $resultats = mysqli_query($conexio, $sql);
+
+    foreach($resultats as $resultat){
+        $id = $resultat['id'];
+    }
+
+    $sql = "SELECT idUsuari FROM administradors WHERE idUsuari=$id";
+
+    $resultat = mysqli_query($conexio, $sql);
+
+?>
+        <div class="col-md-3">
             <p class="benvinguda">BENVINGUT! <?php echo $_SESSION['login_user'] ?></p>
-            <a href="logout.php">Sortir</a>
+            <div class="row">
+                <?php
+                if(mysqli_num_rows($resultat)==1){
+                    echo "<a href='admin.php'>Secció d'Administrador</a>";
+                }    
+                ?>
+            </div>
+            <div class="row">
+                <a href="logout.php">Sortir</a> 
+            </div>  
         </div>
     </nav>
 </header>
